a98a8198136d96c92acbe20dcfe5a4b48f8f5bb5
dhrumit <dhrumittechrabbit@gmail.com>
2026-04-13 14:00:00 +0530

    feat: add testoppvia.vercel.app to CORS allowed origins

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ app.use(
   cors({
     origin: [
       // "http://localhost:3001",
+      "https://testoppvia.vercel.app",
       "https://oppva.netlify.app",
       "http://192.168.1.187:5173",
       "https://talentoppvia.netlify.app",
